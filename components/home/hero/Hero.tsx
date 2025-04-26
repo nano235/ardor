de51c79b9963e63c11495187a304300852d24fcd
nano235 <revivaladolor@yahoo.com>
2025-04-26 15:29:49 +0100

    added demo reel

--- components/home/hero/Hero.tsx
+++ components/home/hero/Hero.tsx
@@ -6,9 +6,16 @@ const Hero = () => {
 	return (
 		<section className={styles.hero}>
 			<div className={styles.container}>
-				<Title className={styles.title} title="We create captivating" />
-				<Title className={styles.title} title="content that drives engagement" />
-				<div className={styles.reel}></div>
+				<Title className={styles.title} title="We Create Captivating Videos" />
+				<Title className={styles.title} title="That Drives Results" />
+				<div className={styles.video}>
+					<video
+						src="https://res.cloudinary.com/dlq93g9xz/video/upload/v1743259889/Ardors_Demo_Reel_coy9zr.mp4"
+						autoPlay
+						muted
+						loop
+					/>
+				</div>
 			</div>
 		</section>
 	);
