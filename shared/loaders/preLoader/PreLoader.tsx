1b275b1dccca9ad99db06df951389d0b1b5a12d2
nano235 <revivaladolor@yahoo.com>
2025-07-02 15:32:23 +0100

    updates

--- shared/loaders/preLoader/PreLoader.tsx
+++ shared/loaders/preLoader/PreLoader.tsx
@@ -3,12 +3,14 @@
 import React from "react";
 import styles from "./PreLoader.module.scss";
 import { useGlobalContext } from "@/context/AppContext";
+import Image from "next/image";
 const PreLoader = () => {
 	const { progress, isPageLoaded } = useGlobalContext();
 	return (
 		<div className={styles.preLoader} data-is-page-loaded={isPageLoaded}>
-			<div className={styles.title}>
-				<h1>ARDORS</h1>
+			<div className={styles.title}>{/* <h1>ARDORS</h1> */}</div>
+			<div className={styles.video}>
+				<Image src="/ardors.gif" fill alt="" />
 			</div>
 			<div className={styles.progress_container}>
 				<div className={styles.text}>
